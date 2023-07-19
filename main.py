83986d13e90cac52d04f9ce3ea5ee395ef5a1b21
XavierFelixan <125172953+XavierFelixan@users.noreply.github.com>
2023-07-19 21:29:21 +0700

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from UI import interface
 from graph import Graph
 
+# make a graph class to make requests to Pixela
 graph = Graph()
+
+# make a GUI class for the user interface
 gui = interface(graph)
